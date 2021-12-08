7878607827cb6eb0180640d26f6f659ddd6a7ad8
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-12-08 17:37:00 +0530

    Add files via upload

---
+++ isbinarytreebstusinginorderproperty.py
@@ -0,0 +1,82 @@
+class btree():
+  def __init__(self, data, right = None, left = None):
+    self.data = data
+    self.right = right
+    self.left = left
+   
+class pair():
+  def __init__(self, node, state):
+    self.node = node
+    self.state = state
+
+
+n = int(input())
+a = []
+for i in range(n):
+  a.append(int(input()))
+ 
+root = btree(a[0])
+stack = []
+stack.append(pair(root,1)) 
+
+idx = 0
+
+ 
+while(len(stack)>0):
+  top = stack[-1]
+  if(top.state ==1):
+    idx += 1
+    if(a[idx]!=-1):
+      t = btree(a[idx])
+      top.node.left = t
+      p = pair(t, 1)
+      stack.append(p)
+    else:
+      top.node.left = None
+    
+    top.state += 1
+    
+  elif(top.state == 2):
+    idx += 1
+    if(a[idx]!=-1):
+      t = btree(a[idx])
+      top.node.right = t
+      p = pair(t, 1)
+      stack.append(p)
+    else:
+      top.node.right = None
+    
+    top.state += 1
+  
+  
+  else:
+    stack.pop()
+
+prev = None
+
+def isbst(root):
+  global prev
+  
+  if(root==None):
+    return True
+  
+  if(isbst(root.left)==False):
+    return False
+    
+  if(prev!=None and prev.data > root.data):
+    return False
+   
+  prev = root
+  
+  if(isbst(root.right)==False):
+    return False 
+    
+  return True
+  
+    
+if(isbst(root)):
+  print("true")
+else:
+  print("false")
+ 
+  
