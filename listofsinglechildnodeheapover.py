3542593b850e60af61c4166e7f958d0baf3d9509
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-12-17 12:36:35 +0530

    Add files via upload

---
+++ listofsinglechildnodeheapover.py
@@ -0,0 +1,70 @@
+class btree():
+  def __init__(self, data, right = None, left = None):
+    self.data = data
+    self.right = right
+    self.left = left
+   
+class pair():
+  def __init__(self, node, state):
+    self.node = node
+    self.state = state
+
+
+n = int(input())
+a = []
+for i in range(n):
+  a.append(int(input()))
+  
+ 
+root = btree(a[0])
+stack = []
+stack.append(pair(root,1)) 
+
+idx = 0
+
+ 
+while(len(stack)>0):
+  top = stack[-1]
+  if(top.state ==1):
+    idx += 1
+    if(a[idx]!=-1):
+      t = btree(a[idx])
+      top.node.left = t
+      p = pair(t, 1)
+      stack.append(p)
+    else:
+      top.node.left = None
+    
+    top.state += 1
+    
+  elif(top.state == 2):
+    idx += 1
+    if(a[idx]!=-1):
+      t = btree(a[idx])
+      top.node.right = t
+      p = pair(t, 1)
+      stack.append(p)
+    else:
+      top.node.right = None
+    
+    top.state += 1
+  
+  
+  else:
+    stack.pop()
+ 
+
+
+def singlechild(node, ans):
+  if((node==None) or (node.right ==None and node.left==None)):
+    return
+   
+  if(node.right==None or node.left==None):
+    ans.append(node.data)
+  
+  singlechild(node.left, ans) 
+  singlechild(node.right, ans)
+ 
+ans = []
+singlechild(root, ans)
+print(*ans)
